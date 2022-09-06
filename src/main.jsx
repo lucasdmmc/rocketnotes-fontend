49089a952ae546bd3a86c512e57dfd5190341592
lucasdmmc <lucas.dmmc@hotmail.com>
2022-09-06 17:56:05 -0300

    i created some components

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom/client'
 import theme from './styles/theme'
 import GlobalStyles from "./styles/global"
 import { ThemeProvider } from 'styled-components'
-import { Details } from './pages/Details'
+import { Home } from './pages/Home'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Details/>
+      <Home/>
     </ThemeProvider>
   </React.StrictMode>
 )
--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,28 +1,40 @@
-import { Container, Links } from "./styles"
+import { Container, Links, Content } from "./styles"
 import { Button } from "../../components/Button"
 import { Header } from "../../components/Header"
 import { Section } from "../../components/Section"
 import { Tag } from "../../components/Tag"
+import { ButtonText } from "../../components/ButtonText"
 
 
 export function Details() {
   return (
     <Container>
       <Header />
-      <Section title="Links Úteis">
-        <Links>
-          <li><a href="#">https://www.rocketseat.com.br</a></li>
-          <li><a href="#">https://www.rocketseat.com.br</a></li>
-        </Links>
-      </Section>
+      
+      <main>
+        <Content>
+          <ButtonText title="Excluir nota" />
 
-      <Section title="Marcadores">
-        <Tag title="Express" />
-        <Tag title="NodeJS" />
-      
-      </Section>
-      
-      <Button name="Voltar"/>
+          <h1>Introdução ao React</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus odit maiores dignissimos eum impedit corporis mollitia iure, tenetur nemo cupiditate debitis beatae veniam placeat quos vel laborum hic quod quo Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet, inventore cumque ratione necessitatibus ipsa magnam nesciunt impedit eligendi cum harum ipsam autem sunt soluta mollitia quidem, fuga perferendis. Architecto, sint!</p>
+
+          <Section title="Links Úteis">
+            <Links>
+              <li><a href="#">https://www.rocketseat.com.br</a></li>
+              <li><a href="#">https://www.rocketseat.com.br</a></li>
+            </Links>
+          </Section>
+
+          <Section title="Marcadores">
+            <Tag title="Express" />
+            <Tag title="NodeJS" />
+
+          </Section>
+
+          <Button name="Voltar"/>
+        
+        </Content>
+      </main>
     </Container>
   )
 }
