258f366f8fcd7d03e9ae6aa1bd9740c88276b486
Lucas Carvalho <104842709+lucasdmmc@users.noreply.github.com>
2022-09-06 18:07:04 -0300

    I organized some pending

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,36 @@
+import { FiPlus, FiSearch} from "react-icons/fi"
+import { Container, Brand, Menu, Search, Content, NewNote } from "./styles"
+import { Header } from "../../components/Header"
+import { ButtonText } from "../../components/ButtonText"
+import { Input } from "../../components/Input"
+
+export function Home() {
+  return (
+    <Container>
+      <Brand>
+        <h1>RocketNotes</h1>
+      </Brand>
+
+      <Header />
+
+      <Menu>
+        <li><ButtonText title="Todos"/></li>
+        <li><ButtonText title="ReactJS"/></li>
+        <li><ButtonText title="NodeJS"/></li>
+      </Menu>
+
+      <Search>
+        <Input placeholder="Pesquisar pelo título" icon={FiSearch}/>
+      </Search>
+
+      <Content>
+
+      </Content>
+
+      <NewNote>
+        <FiPlus />
+        Criar conta
+      </NewNote>
+    </Container>
+  )
+}
