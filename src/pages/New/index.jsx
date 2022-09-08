c3d7ee9128164b2038d38901517b7eb478798598
lucasdmmc <lucas.dmmc@hotmail.com>
2022-09-08 13:44:06 -0300

    I created components and finished all components

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -1,6 +1,10 @@
 import { Container, Form } from "./styles"
 import { Input } from "../../Components/Input"
 import { Header } from "../../Components/Header"
+import { Textarea } from "../../Components/Textarea"
+import { Section } from "../../Components/Section"
+import { NoteItem } from "../../Components/NoteItem"
+import { Button } from "../../Components/Button"
 
 export function New() {
   return(
@@ -9,10 +13,30 @@ export function New() {
 
       <main>
         <Form>
-          <h1>Criar nota</h1>
-          <a href="#">Voltar</a>
+          <header>
+            <h1>Criar nota</h1>
+            <a href="#">Voltar</a>
+          </header>
 
           <Input placeholder="Titulo" />
+
+          <Textarea placeholder="Observações" />
+
+          <Section title="Links uteis">
+            <NoteItem value="https://www.rocketseat.com.br" />
+            <NoteItem 
+              isNew 
+              placeholder="Novo link" 
+            />
+          </Section>
+
+          <Section title="Marcadores">
+            <div className="tags">
+              <NoteItem value="React" />
+              <NoteItem isNew placeholder="Nova tag" />
+            </div>
+          </Section>
+          <Button title="Salvar" />
         </Form>
       </main>
     </Container>
--- src/pages/New/styles.js
+++ src/pages/New/styles.js
@@ -8,10 +8,35 @@ export const Container = styled.div`
   grid-template-rows: 105px auto;
   grid-template-areas: 
   "header"
-  "content"
-  ;
+  "content";
+
+  > main {
+    grid-area: content;
+    overflow-y: auto;
+  }
+
+  .tags {
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+  }
 `
 
 export const Form = styled.form`
-  
+  max-width: 550px;
+  margin: 38px auto;
+
+  > header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    margin-bottom: 38px;
+
+    a {
+      font-size: 20px;
+      color: ${({ theme }) => theme.COLORS.GRAY_100}
+    }
+  }
+
 `
