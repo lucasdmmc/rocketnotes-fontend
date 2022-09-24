06545c61f9621810d81a1fc101dd9d0dec45bdd3
lucasdmmc <lucas.dmmc@hotmail.com>
2022-09-24 09:54:02 -0300

    Adicionei logout na aplicação

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,7 +1,9 @@
 import { RiShutDownLine } from "react-icons/ri"
 import { Container, Profile, Logout } from "./styles"
+import { useAuth } from "../../hooks/auth"
 
 export function Header() {
+  const { signOut } = useAuth()
   return (
     <Container>
       <Profile to="/profile">
@@ -16,7 +18,7 @@ export function Header() {
         </div>
       </Profile>
 
-      <Logout to="/">
+      <Logout onClick={signOut}>
         <RiShutDownLine />
       </Logout>
     </Container>
